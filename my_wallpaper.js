afc092dac58dd70b450bb1816b095d18703f7a1e
Lma00ee <lakhena.mao0@gmail.com>
2024-07-27 17:59:08 +1200

    Update my_wallpaper.js

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -1,20 +1,8 @@
 //your parameter variables go here!
-let circleX = 100
-let circleY = 100
-let circleS = 50 //head size 
-
-let earX = circleX-20
-let earY = circleY-20
-let earS = 20 //ear size 
-
-let eyeX = circleX-10
-let eyeY = circleY
-let eyeS = 5 //eye size 
-
-let noseX = 100
-let noseY = noseX
-
-
+  let circleX = 100
+  let circleY = 100
+  let circleS = 50 //head size 
+  
 
 function setup_wallpaper(pWallpaper) {
   pWallpaper.output_mode(DEVELOP_GLYPH);
@@ -32,7 +20,27 @@ function wallpaper_background() {
 }
 
 function my_symbol() { // do not rename this function. Treat this similarly to a Draw function
+  bearHead(circleX, circleY, circleS);
+  bearHead (50, 50, 50)
+  bearHead (150, 150, 50)
+
+
+
+
+
+}
+function bearHead (circleX, circleY, circleS){ 
+
+  let noseX = circleX
+  let noseY = noseX
+  let earX = circleX-20
+  let earY = circleY-20
+  let earS = 20 //ear size 
   
+  
+  let eyeX = circleX-10
+  let eyeY = circleY
+  let eyeS = 5 //eye size 
 //ear
 stroke(1);
 fill (105, 89, 83); //brown 
@@ -56,11 +64,22 @@ circle (earX+40, earY, earS-10) //right ear, earS10
   line (noseX, noseY+5,noseX-5, noseY+10); //left nose line 100, 105, 95, 110
   line (noseX, noseY+5, noseX+5, noseY+10); //right nose line 100, 105, 105, 110
 
-//eyes
+// eyes
 stroke(1)
 fill (0);
 circle (eyeX, eyeY, eyeS) //left eye 
 circle (eyeX+20, eyeY, eyeS) //right eye, eyeX 110
 
+// stroke(1);
+// line (85, 100, 90, 100);
+// line (110, 100, 115, 100);
 
 }
+
+
+
+
+
+
+
+
