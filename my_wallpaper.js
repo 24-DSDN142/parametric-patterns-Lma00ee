0e269bad4d5378b48d8beedb7ff2c159ae9d64a1
Lma00ee <lakhena.mao0@gmail.com>
2024-07-29 15:59:01 +1200

    Update my_wallpaper.js

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -31,6 +31,7 @@ function wallpaper_background() {
 
 function my_symbol() { // do not rename this function. Treat this similarly to a Draw function
   bearHead(circleX, circleY);
+  bearHead(100, 150);
   // bearHead (50, 50,)
   // bearHead (150, 150)
   bow(bowX, bowY);
@@ -50,6 +51,7 @@ function bow (bowX, bowY) {
   // rotate (-20)
 
   beginShape()
+ stroke(1);
   fill (255, 0, 0); //red for now
   curveVertex (bowX, bowY-25);
   curveVertex (bowX, bowY-25)
@@ -76,8 +78,10 @@ function bearHead (circleX, circleY){
   let earX = circleX-20
   let earY = circleY-20
   let earS = 20 //ear size 
-  
-  
+
+  let noseX = circleX
+  let noseY = circleY
+
   let eyeX = circleX-10
   let eyeY = circleY
   let eyeS = 5 //eye size 
@@ -99,11 +103,54 @@ circle (earX+40, earY, earS-10) //right ear, earS10
   fill (105, 89, 83); //brown
   circle (circleX, circleY, circleS); // head overlapped body
   
-  //nose 
+
+  // straight nose 
+  // stroke(1);
+  // line (circleX, circleY, circleX, circleY+5); //straight nose line 100, 100, 100, 105
+  // line (circleX, circleY+5,circleX-5, circleY+9); //left nose line 100, 105, 95, 110
+  // line (circleX, circleY+5, circleX+5, circleY+9); //right nose line 100, 105, 105, 110
+
+  //curved nose 
+//straight nose line for curved nose 
   stroke(1);
-  line (circleX, circleY, circleX, circleY+5); //straight nose line 100, 100, 100, 105
-  line (circleX, circleY+5,circleX-5, circleY+10); //left nose line 100, 105, 95, 110
-  line (circleX, circleY+5, circleX+5, circleY+10); //right nose line 100, 105, 105, 110
+  line (noseX, noseY+3, noseX, noseY+5);
+
+//left curved nose line 
+  beginShape(line)
+  stroke(1)
+  fill (105, 89, 83);
+curveVertex (noseX-10,noseY-15);
+curveVertex(noseX-5, noseY+9);
+curveVertex (noseX, noseY+5);
+curveVertex (noseX-10, noseY-10);
+endShape()
+
+//right curved nose line 
+beginShape(line)
+stroke(1)
+fill (105, 89, 83);
+curveVertex (noseX+10, noseY-15);
+curveVertex(noseX+5, noseY+9);
+curveVertex (noseX, noseY+5);
+curveVertex (noseX+10, noseY-10);
+endShape()
+
+//curved triangular nose
+beginShape()
+stroke(1);
+fill (255)
+curveVertex (noseX, noseY+5)
+curveVertex (noseX-3, noseY+2)
+curveVertex (noseX+3, noseY+2)
+curveVertex (noseX, noseY+5)
+endShape(CLOSE)
+
+// //circle nose 
+// stroke(1);
+// fill (255);
+// circle (noseX, noseY+3, 4)
+
+
 
 // eyes
 stroke(1)
@@ -111,6 +158,13 @@ fill (0);
 circle (eyeX, eyeY, eyeS) //left eye 
 circle (eyeX+20, eyeY, eyeS) //right eye, eyeX 110
 
+//white inside of eyes 
+noStroke()
+fill(255);
+circle (eyeX-1, eyeY-1, eyeS-3)
+circle (eyeX+19, eyeY-1, eyeS-3) //right eye, eyeX 11
+
+//closed eyes 
 // stroke(1);
 // line (85, 100, 90, 100);
 // line (110, 100, 115, 100);
