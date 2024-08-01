7a9ad31082a32c17e7d079f961a52857bd78297e
Lma00ee <lakhena.mao0@gmail.com>
2024-08-01 15:40:52 +1200

    Final coding assignment version!
    Final coding and assignment version w all wallpaper parameters in settings.txt file.

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -7,64 +7,66 @@
   let bowY = bowX //bow on y-axis, 100
   let bowCircle = 5 //bow circle/middle size
 
-  let pinkbackground = true //pink background with pink checkered pattern, if turned off changes to red checkered pattern + background colour 2 
+  //colour variables 
 
-  let bgcolor1 = ('#f7c8b5') //light pink background colour, swtiches on when pink checkered pattern
-  let bgcolor2 = ('#f0ceb1') //lighter cream colour, switches on when red checkered pattern 
+  let bgColour1 = ('#f7c8b5') //light pink background colour, swtiches on when pink checkered pattern
+  let bgColour2 = ('#f0ceb1') //lighter cream colour, switches on when red checkered pattern 
 
-  let blush = true //controls if there is blush on the cheeks of bear or not
-  let blushpink = ('#f25060') //pink blush (for lighter brown bear )
-  let blushred = ('#b51818') //red blush (for darker brown bear)
+  let blushPink = ('#f25060') //pink blush (for lighter brown bear )
+  let blushRed = ('#b51818') //red blush (for darker brown bear)
 
-  let bearfacelightbrown = ('#cf9974') //light brown milk tea colour of the bear 
-  let bearfacedarkbrown = ('#8f5935') //darker chocolate brown colour of the bear 
+  let bearFaceLightBrown = ('#cf9974') //light brown milk tea colour of the bear 
+  let bearFaceDarkBrown = ('#8f5935') //darker chocolate brown colour of the bear 
 
-  let innerear1 = ('#b57659') //inner ear colour for light brown bear (lighter)
-  let innerear2 = ('#5e361a') //inner ear colour for dark brown bear (darker)
+  let innerEar1 = ('#b57659') //inner ear colour for light brown bear (lighter)
+  let innerEar2 = ('#5e361a') //inner ear colour for dark brown bear (darker)
   
-  let lightbrownsnout = ('#f7d7ad') //snout colour for light brown bear (lighter)
-  let darkbrownsnout = ('#e0a880') //snout colour for dark brown bear (darker)
+  let lightBrownSnout = ('#f7d7ad') //snout colour for light brown bear (lighter)
+  let darkBrownSnout = ('#e0a880') //snout colour for dark brown bear (darker)
 
   let bowRed = ('#d42f2f') //dark red bow colour (better for darker bear)  
   let bowPink = ('#f74561') //pink bow colour (better for lighter bear)
  
-  let bearExist = 1 
+  //control variables/parameters 
 
-  let triangularNose = true //controls if bear has an ellipse round nose or triangle nose 
+  let pinkBackground = true //pink background with pink checkered pattern, if turned off changes to red checkered pattern + background colour 2 
+
+  let bearExist = 1 //lets middle, top left or bottom right bears exist
+
+  let blush = true //controls if there is blush on the cheeks of bear or not
+
+  let triangularNose = false //controls if bear has an ellipse round nose or triangle nose 
 
   let eyesOpen = true //controls if bear has open or closed eyes 
 
-  let bearDark = true //controls if the colour of the bear is dark or light
+  let bearDark = false //controls if the colour of the bear is dark or light
 
-  let bowSwitch = true //controls the colours of the bow/switches them between red and pink!
+  let bowSwitch = false //True = red, false= pink. controls the colours of the bow/switches them between red and pink. 
  
-  let bow1 = false
-  let bow2 = false
-  let bow3 = true
-  let bow4 = false
+  let bow1 = true //switches on head bow
+  let bow2 = false //switches on neck bow
+  let bow3 = false //switches on head and neck bow
 
 function setup_wallpaper(pWallpaper) {
-  // pWallpaper.output_mode(GRID_WALLPAPER);
-  // pWallpaper.resolution(NINE_PORTRAIT);
-  pWallpaper.output_mode(DEVELOP_GLYPH);
-  pWallpaper.resolution(FIT_TO_SCREEN);
+  pWallpaper.output_mode(GRID_WALLPAPER);
+  pWallpaper.resolution(NINE_PORTRAIT);
   pWallpaper.show_guide(false); //set this to false when you're ready to print
 
   //Grid settings
-  pWallpaper.grid_settings.cell_width  = 180;
+  pWallpaper.grid_settings.cell_width  = 200;
   pWallpaper.grid_settings.cell_height = 200;
-  pWallpaper.grid_settings.row_offset  = 180;
+  pWallpaper.grid_settings.row_offset  = 0;
 
   angleMode(DEGREES)
 }
 
 function wallpaper_background() {
- if (pinkbackground) { 
-  background(bgcolor1); 
+ if (pinkBackground) { 
+  background(bgColour1); //light pink (for pink checkered pattern)
  }
 
  else {
-background (bgcolor2);
+background(bgColour2); //cream colour (for red checkered pattern)
  }
 
 }  
@@ -73,15 +75,15 @@ function my_symbol() { // do not rename this function. Treat this similarly to a
 
 //checkered background, transparent  
  noStroke()
- if (pinkbackground) {
+ if (pinkBackground) {
  fill (255, 112, 133, 70) //light pink, slightly transparent
  }
 
  else {
-fill (237, 36, 36, 70) //red transparent 
+fill (237, 36, 36, 70) //red, transparent 
  }
 
- rect (25, 0, 35, 200) // left vertical line 
+ rect (25, 0, 35, 200) //left vertical line 
  rect (140, 0, 35, 200) //right vertical line
  rect (0, 30, 200, 35) //top horizontal line
  rect (0, 130, 200, 35) //bottom horizontal line
@@ -90,112 +92,77 @@ fill (237, 36, 36, 70) //red transparent
   
  if (bearExist == 1) {
  //middle bear head 
-  bearHeadDark (circleX, circleY); // middle bear head 
- }
+  bearHead (circleX, circleY); //100, 100
 
  if (bow1) {
-  bow(bowX, bowY); // headbow of middle bear 
+  bow(bowX, bowY); //headbow of middle bear, 100, 100
  }
  else if (bow2) {
-  bow (bowX, bowY+50); // neckbow
+  bow (bowX, bowY+50); //neckbow, 100, 150
  }
  else if (bow3) {
-  bow(bowX, bowY); // headbow of middle bear 
-  bow (bowX, bowY+50); // neckbow
+  //both head and neck bow 
+  bow(bowX, bowY); //headbow of middle bear, 100, 100 
+  bow (bowX, bowY+50); //neckbow, 100, 150 
  }
- else if (bow4) {
- //bows on either side of head 
-  //left bow 
-  rotate(-20)
-  bow(45, 135) 
 
-  //right bow 
-  rotate(40)
-  bow (145, 65)
- }
 else {
 
 }
+
+ }
 
 //
 
- if (bearExist == 1) {
+ if (bearExist == 2) {
   //top left bear head 
-  bearHeadDark (50, 50) 
- }
+  bearHead (50, 50) 
+ 
   if (bow1) {
-  bow(bowX-50, bowY-50); // headbow of middle bear 
+  bow(bowX-50, bowY-50); //headbow of middle bear, 50, 50 
  }
  else if (bow2) {
-  bow (bowX-50, bowY); // neckbow
+  bow (bowX-50, bowY); //neckbow, 50, 100
  }
  else if (bow3) {
   //both the head and neck bow 
-  bow(bowX-50, bowY-50); // headbow of middle bear 
-  bow (bowX-50, bowY); // neckbow
- }
- else if (bow4) {
- //bows on either side of head 
-  //left bow 
-  rotate(-20)
-  bow(45, 135) 
-
-  //right bow 
-  rotate(40)
-  bow (145, 65)
+  bow(bowX-50, bowY-50); //headbow of middle bear, 50, 50 
+  bow (bowX-50, bowY); //neckbow, 50, 100
  }
 else {
 
 
 }
 
- if (bearExist == 1) {
+ }
+
+//
+
+ if (bearExist == 2) {
   //bottom right bear head
-  bearHeadDark (150, 150); // bottom right bear head 
- }
+  bearHead (150, 150); 
+
  if (bow1) {
-  bow(bowX+50, bowY+50); // headbow of middle bear 
+  bow(bowX+50, bowY+50); //headbow of middle bear, 150, 150
  }
  else if (bow2) {
-  bow (bowX+50, bowY+100); // neckbow
+  bow (bowX+50, bowY+100); //neckbow, 150, 200
  }
  else if (bow3) {
   //both the head and neck bow 
-  bow(bowX+50, bowY+50); // headbow of middle bear 
-  bow (bowX+50, bowY+100); // neckbow
- }
- else if (bow4) {
- //bows on either side of head 
-  //left bow 
-  rotate(-20)
-  bow(45, 135) 
-
-  //right bow 
-  rotate(40)
-  bow (145, 65)
+  bow(bowX+50, bowY+50); //headbow of middle bear, 150, 150 
+  bow (bowX+50, bowY+100); //neckbow, 150, 200
  }
 else {
 
 
 }
-  
 
- if (false) {
-  //bows on either side of head 
-  //left bow 
-  rotate(-20)
-  bow(45, 135); 
-
-  //right bow 
-  rotate(40)
-  bow (145, 65)
-
-  }
+}
 
 }
 
 function bow (bowX, bowY) {
-  //right bow 
   beginShape()
   stroke(1);
   if (bowSwitch) {
@@ -208,10 +175,11 @@ function bow (bowX, bowY) {
 
   }
 
-  curveVertex (bowX, bowY-25); // bowY=75
-  curveVertex (bowX, bowY-25) // bowY=75
-  curveVertex (bowX+10, bowY-30);
-  curveVertex (bowX+10, bowY-20);
+  //right bow 
+  curveVertex (bowX, bowY-25); //bowY=75
+  curveVertex (bowX, bowY-25) //bowY=75
+  curveVertex (bowX+10, bowY-30); //110, 70
+  curveVertex (bowX+10, bowY-20); //110, 80 
   endShape(CLOSE)
   
   //left bow 
@@ -219,7 +187,7 @@ function bow (bowX, bowY) {
   curveVertex (bowX, bowY-25); // bowY=75
   curveVertex (bowX, bowY-25); // bowY=75
   curveVertex (bowX-10, bowY-30); // bowX=90, bowY=70
-  curveVertex (bowX-10, bowY-20); //bowX=90, bowY=70
+  curveVertex (bowX-10, bowY-20); //bowX=90, bowY=80
   endShape(CLOSE)
   
   //bow circle 
@@ -230,14 +198,14 @@ function bow (bowX, bowY) {
 
   
 
-function bearHeadDark (circleX, circleY){ 
+function bearHead (circleX, circleY) { 
 
   let earX = circleX-20 //80, size of ear on x-axis
   let earY = circleY-20 //80, size of ear on y-axis 
   let earS = 20 //ear size
 
-  let noseX = circleX //nose size x-axis
-  let noseY = circleY //nose size y-axis 
+  let noseX = circleX //nose size x-axis, 100
+  let noseY = circleY //nose size y-axis, 100  
 
   let eyeX = circleX-10 //90, eye size on x-axis 
   let eyeY = circleY //eye size on y-axis, 100 
@@ -249,10 +217,10 @@ function bearHeadDark (circleX, circleY){
   let ellipseW = circleS-43 //7, elipse/blush width
   let ellipseH = 4 //elipse/blush height 
   
-//ear
+
 stroke(1);
 if (bearDark) {
-fill (bearfacedarkbrown); //same colour/brown of the bear/face
+fill (bearFaceDarkBrown); //same colour/brown of the bear/face
 
 
 circle (earX, earY, earS) //left ear
@@ -260,30 +228,31 @@ circle (earX+40, earY, earS) //right ear, earX=120
 
 //inner ear 
 noStroke();
-fill (innerear2); //slightly darker brown inner ear 
+fill (innerEar2); //slightly darker brown inner ear 
 circle (earX, earY, earS-10) //left ear, earS=10 
-circle (earX+40, earY, earS-10) //right ear, earX=120, earS10 
+circle (earX+40, earY, earS-10) //right ear, earX=120, earS=10 
 
 //head  
 stroke(1);
-fill (bearfacedarkbrown); //brown
-circle (circleX, circleY, circleS); // head overlapped body, circleX=100, circleY=100, circleS=50
+fill (bearFaceDarkBrown); 
+circle (circleX, circleY, circleS); //circleX=100, circleY=100, circleS=50
  
-// lighter-coloured rounded nose snout circle 
+//rounded nose snout circle 
 noStroke();
-fill(darkbrownsnout); //dark brown
+fill(darkBrownSnout); 
 circle (noseX, noseY+6, 15) //noseX=100, noseY=106, 15
 
 
-//   curved nose 
-// straight nose line for curved nose 
+// curved nose 
+
+//straight nose line for curved nose 
 stroke(1);
 line (noseX, noseY+3, noseX, noseY+5); //100, 103, 100, 105
 
 //left curved nose line 
 beginShape(line)
 stroke(1)
-fill (darkbrownsnout); //dark brown, same as nose snout colour
+fill (darkBrownSnout); //dark brown, same as nose snout colour
 curveVertex (noseX-10,noseY-15); //90, 85
 curveVertex(noseX-5, noseY+9); //95, 91
 curveVertex (noseX, noseY+5); //100, 105
@@ -293,7 +262,7 @@ endShape()
 //right curved nose line 
 beginShape(line)
 stroke(1)
-fill (darkbrownsnout); //dark brown, same as nose snout colour
+fill (darkBrownSnout); //dark brown, same as nose snout colour
 curveVertex (noseX+10, noseY-15); //110, 85
 curveVertex(noseX+5, noseY+9); //105, 109
 curveVertex (noseX, noseY+5); //100, 105
@@ -325,7 +294,7 @@ ellipse (ellipseX+13, ellipseY-2, 6, 3)
 if (blush){
 //blush on cheeks
 noStroke()
-fill(blushred) //red 
+fill(blushRed) //red 
 ellipse (ellipseX, ellipseY, ellipseW, ellipseH) // left, 87, 106, 7, 4 
 ellipse (ellipseX+26, ellipseY, ellipseW, ellipseH) // right 113, 106, 7, 4
 
@@ -366,25 +335,25 @@ else {
 
 //ear
 stroke(1);
-fill (bearfacelightbrown); //same colour/brown of the bear/face
+fill (bearFaceLightBrown); //same colour/brown of the bear/face
 circle (earX, earY, earS) //left ear
 circle (earX+40, earY, earS) //right ear, earX=120
 
 //inner ear 
 noStroke();
-fill (innerear1); //slightly darker brown inner ear 
+fill (innerEar1); //slightly darker brown inner ear 
 circle (earX, earY, earS-10) //left ear, earS=10 
 circle (earX+40, earY, earS-10) //right ear, earX=120, earS10 
 
 //head  
 stroke(1);
-fill (bearfacelightbrown); //brown
+fill (bearFaceLightBrown); //brown
 circle (circleX, circleY, circleS); // head overlapped body, circleX=100, circleY=100, circleS=50
  
 if (true) {
 // lighter-coloured rounded nose snout circle 
 noStroke();
-fill(lightbrownsnout); //light beigish creamish brown
+fill(lightBrownSnout); //light beigish creamish brown
 circle (noseX, noseY+6, 15) //noseX=100, noseY=106, 15
 
 }
@@ -398,9 +367,9 @@ line (noseX, noseY+3, noseX, noseY+5); //100, 103, 100, 105
 //left curved nose line 
 beginShape(line)
 stroke(1)
-fill (lightbrownsnout); //light beigish creamish brown, same as nose snout colour
+fill (lightBrownSnout); //light beigish creamish brown, same as nose snout colour
 curveVertex (noseX-10,noseY-15); //90, 85
-curveVertex(noseX-5, noseY+9); //95, 91
+curveVertex(noseX-5, noseY+9); //95, 109
 curveVertex (noseX, noseY+5); //100, 105
 curveVertex (noseX-10, noseY-10); //90, 90
 endShape()
@@ -408,7 +377,7 @@ endShape()
 //right curved nose line 
 beginShape(line)
 stroke(1)
-fill (lightbrownsnout); //light beigish creamish brown, same as nose snout colour
+fill (lightBrownSnout); //light beigish creamish brown, same as nose snout colour
 curveVertex (noseX+10, noseY-15); //110, 85
 curveVertex(noseX+5, noseY+9); //105, 109
 curveVertex (noseX, noseY+5); //100, 105
@@ -417,6 +386,7 @@ endShape()
 
 
 if (triangularNose) {
+
 //curved triangular nose, downwards
 beginShape()
 stroke(1);
@@ -438,11 +408,12 @@ ellipse (ellipseX+13, ellipseY-2, 6, 3)
 }
 
 if (blush){
+
 //blush on cheeks
 noStroke()
-fill(blushpink) //pink 
-ellipse (ellipseX, ellipseY, ellipseW, ellipseH) // left, 87, 106, 7, 4 
-ellipse (ellipseX+26, ellipseY, ellipseW, ellipseH) // right 113, 106, 7, 4
+fill(blushPink) //pink 
+ellipse (ellipseX, ellipseY, ellipseW, ellipseH) //left, 87, 106, 7, 4 
+ellipse (ellipseX+26, ellipseY, ellipseW, ellipseH) //right 113, 106, 7, 4
 
 }
 
